3834c8131577461adce8433bb09baa55905b5d42
karthur <1104036840@qq.com>
2021-03-02 18:01:10 +0800

    2

--- parser.cpp
+++ parser.cpp
@@ -5,22 +5,40 @@
 #include<regex>
 #include<string>
 #include<fstream>
+using namespace std;
 #define regexIgnore "(\\s)*;(.)*"
 #define regexG01 "G01|G1"
-#define regexX "X(((\\+|-)?([0-9]+)(\\.[0-9]+)?)|((\\+|-)?\\.?[0-9]+))"
-#define regexY "Y(((\\+|-)?([0-9]+)(\\.[0-9]+)?)|((\\+|-)?\\.?[0-9]+))"
-#define regexZ "Z(((\\+|-)?([0-9]+)(\\.[0-9]+)?)|((\\+|-)?\\.?[0-9]+))"
+const char* regexX = R"(X[-+]?[0-9]*\.?[0-9]+)";
+const char* regexY = R"(Y[-+]?[0-9]*\.?[0-9]+)";
+const char* regexZ = R"(Z[-+]?[0-9]*\.?[0-9]+)";
 #define regexF "F(((\\+|-)?([0-9]+)(\\.[0-9]+)?)|((\\+|-)?\\.?[0-9]+))"
-const std::regex commentReg(regexIgnore);
-const std::regex xReg(regexX);
-const std::regex yReg(regexY);
-const std::regex g01Reg(regexG01);
-const std::regex zReg(regexZ);
-const std::regex fReg(regexF);
-std::string str("G1 X34.302 Y41.626 F1800.000");
+const regex commentReg(regexIgnore);
+const regex xReg(regexX);
+const regex yReg(regexY);
+const regex g01Reg(regexG01);
+const regex zReg(regexZ);
+const regex fReg(regexF);
+string str("G1 X34.302 Y41.626 F1800.000");
 int main()
 {
-	std::smatch match;
-	std::cout << std::regex_search(str, match, yReg) << "\n";
-	std::cout << match[1].str().erase(0, 0);
+    fstream newfile;
+    vector<const char*> results(100000,"");
+    
+    newfile.open("test.gcode", ios::in); //open a file to perform read operation using file object
+    if (newfile.is_open()) {   //checking whether the file is open
+        string line;
+        int count = 0;
+        smatch temp;//存储string match结果
+        while (getline(newfile, line)) { //read data from file object and put it into string.
+            
+            if (regex_search(line,temp, zReg))
+            {
+                cout << temp[0].str().erase(0,1) << '\t' << count++ << "\n"; //print the data of the string
+            }
+        }
+        newfile.close(); //close the file object.
+    }
+	/*smatch match;
+	cout << std::regex_search(str, match, yReg) << "\n";
+	cout << match[1].str().erase(0, 0);*/
 }
