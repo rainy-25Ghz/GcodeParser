6240cd3754a7e5618b337d547a84231f1c6016e8
karthur <1104036840@qq.com>
2021-03-02 16:20:44 +0800

    2

--- parser.cpp
+++ parser.cpp
@@ -4,6 +4,7 @@
 #include<iostream>
 #include<regex>
 #include<string>
+#include<fstream>
 #define regexIgnore "(\\s)*;(.)*"
 #define regexG01 "G01|G1"
 #define regexX "X(((\\+|-)?([0-9]+)(\\.[0-9]+)?)|((\\+|-)?\\.?[0-9]+))"
@@ -20,6 +21,6 @@ std::string str("G1 X34.302 Y41.626 F1800.000");
 int main()
 {
 	std::smatch match;
-	std::cout << std::regex_search(str, match, regy) << "\n";
+	std::cout << std::regex_search(str, match, yReg) << "\n";
 	std::cout << match[1].str().erase(0, 0);
 }
